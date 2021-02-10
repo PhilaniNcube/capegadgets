af6d759d2243aae1d76630123284d412bd4645b8
Philani Ncube <ncbphi001@gmail.com>
2021-02-10 21:30:01 +0200

    paypal payments

---
+++ backend/routers/userRouter.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import expressAsycnHandler from 'express-async-handler';
+import bcrypt from 'bcryptjs';
+import data from '../data.js';
+import User from '../models/userModel.js';
+import { generateToken } from '../utils.js';
+import expressAsyncHandler from 'express-async-handler';
+
+const userRouter = express.Router();
+
+userRouter.get(
+  '/seed',
+  expressAsycnHandler(async (req, res) => {
+    // await User.deleteMany({});
+    const createdUsers = await User.insertMany(data.users);
+    res.send(createdUsers);
+  }),
+);
+
+userRouter.post(
+  '/signin',
+  expressAsycnHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: 'Invalid email or password' });
+  }),
+);
+
+userRouter.post(
+  '/register',
+  expressAsyncHandler(async (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
+    const createdUser = await user.save();
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(createdUser),
+    });
+  }),
+);
+
+export default userRouter;
